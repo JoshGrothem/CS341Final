2e01367a7d25b4569c6a820aeab2e7c36892dea1
Joshua Grothem <joshua.grothem@gmail.com>
2022-12-02 13:45:38 -0800

    Created.

---
+++ CS341_Final_Project/src/Type_B_GameObject_adaptor.java
@@ -0,0 +1,10 @@
+
+public abstract class Type_B_GameObject_adaptor extends GameObject {
+	GameObject gObj;
+
+	public Type_B_GameObject_adaptor(GameObject gObj) {
+		super(gObj.getX(), gObj.getY());
+	}
+
+	public abstract void move(Canvas c);
+}
